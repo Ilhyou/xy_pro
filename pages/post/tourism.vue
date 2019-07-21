8471b687a5c5b57812e2f43d78f46c64fc84bd6d
ccc949352432 <51562495+ccc949352432@users.noreply.github.com>
2019-07-21 17:14:30 +0800

    旅游想请1.0
    旅游详情主页面

---
+++ pages/post/tourism.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="contianer">
+    <el-row type="flex" justify="space-between">
+      <!-- 左部详情页 -->
+      <div class="main">
+        <TravelDetails />
+      </div>
+      <div class="aside">
+        <Correlative/>
+      </div>
+    </el-row>
+  </div>
+</template>
+
+<script>
+import Correlative from '@/components/post/Correlative.vue'
+import TravelDetails from '@/components/post/travelDetails.vue'
+export default {
+  components: {
+    TravelDetails,
+    Correlative
+  }
+
+}
+</script>
+
+<style scoped lang="less">
+    .contianer{
+        width: 1000px;
+        margin: 0 auto;
+        padding-top: 20px;
+
+       .aside{
+            width: 280px;
+       }
+    }
+
+</style>
