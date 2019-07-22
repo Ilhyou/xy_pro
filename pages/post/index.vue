cbdc1769e44c35e7c7110a14714d9c9073d45d9e
lanmengting <1044512563@qq.com>
2019-07-22 14:12:07 +0800

    写游记

--- pages/post/index.vue
+++ pages/post/index.vue
@@ -1,69 +1,32 @@
 <template>
-  <div style="padding:50px;">
-     <h1 style="margin-bottom:50px;">组件递归</h1>
-
-    <NavsItem :data="navs"/>
-
-  </div>
+    <div style="padding:50px;">
+        <el-button type="primary" size="mini" @click="$router.push({path: '/post/create'})" >写游记</el-button>
+       
+    </div>
 </template>
 
 <script>
 
-import NavsItem from "@/components/post/navsItem";
-
 export default {
     components: {
-        NavsItem,
     },
-    data(){
+    data() {
         return {
-            navs: [
-                {
-                    title: "衣服",
-                    children: [
-                        { 
-                            title: "男装",
-                            children: [
-                                { 
-                                    title: "T恤",
-                                    children: [ 
-                                        {title: "短袖"}, 
-                                        { title: "长袖" }
-                                     ]
-                                }
-                            ]
-                        },
-                        { 
-                            title: "女装",
-                            children: [
-                                {
-                                    title: "裙子",
-                                    children: [
-                                        { title: "连衣裙" },
-                                        { title: "短裙" }
-                                    ]
-                                }
-                            ]
-                        }
-                    ]
-                },
-                {
-                    title: "电器",
-                    children: [
-                        { title: "电视", children: [
-                            { title: "液晶电视" },
-                            { title: "普通电视"}
-                        ] },
-                        { title: "冰箱" },
-                        { title: "空调" },
-                    ]
-                }
-            ]
-        }
+            list:{
+            }
+        };
+    },
+    mounted(){
+        this.$axios({
+            url:'/posts',
+            method: "GET",
+        }).then(res=>{
+            console.log(res);
+            this.list= res.data
+        })
     }
-}
+};
 </script>
 
 <style>
-
 </style>
