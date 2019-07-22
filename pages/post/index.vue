3c7a4ab1e7af345abd8406a21a442fe6ddc5d29d
lanmengting <1044512563@qq.com>
2019-07-22 15:21:52 +0800

    修改游记

--- pages/post/index.vue
+++ pages/post/index.vue
@@ -1,42 +1,10 @@
 <template>
-<<<<<<< HEAD
-    <div style="padding:50px;">
-        <el-button type="primary" size="mini" @click="$router.push({path: '/post/create'})" >写游记</el-button>
-       
+    <div>
+        <div class="box">
+            <Postleft :data="navs" />
+            <Postright />
+        </div>
     </div>
-</template>
-
-<script>
-
-export default {
-    components: {
-    },
-    data() {
-        return {
-            list:{
-            }
-        };
-    },
-    mounted(){
-        this.$axios({
-            url:'/posts',
-            method: "GET",
-        }).then(res=>{
-            console.log(res);
-            this.list= res.data
-        })
-    }
-};
-</script>
-
-<style>
-=======
-  <div>
-    <div class="box">
-      <Postleft :data="navs" />
-      <Postright />
-    </div>
-  </div>
 </template>
 
 <script>
@@ -44,42 +12,41 @@ import Postleft from "@/components/post/postleft";
 import Postright from "@/components/post/postright";
 
 export default {
-  components: {
-    Postleft,
-    Postright
-  },
-  data() {
-    return {
-      navs: []
-    };
-  },
-  mounted() {
-    // 把axios绑定到原型
-    // Vue.prototype.$axios = axios
-    this.$axios({
-      url: "/posts/cities",
-      method: "GET"
-    }).then(res => {
-      // console.log(res);
-      const { data } = res.data;
-      this.navs = data;
-    });
-  }
+    components: {
+        Postleft,
+        Postright
+    },
+    data() {
+        return {
+            navs: []
+        };
+    },
+    mounted() {
+        // 把axios绑定到原型
+        // Vue.prototype.$axios = axios
+        this.$axios({
+            url: "/posts/cities",
+            method: "GET"
+        }).then(res => {
+            // console.log(res);
+            const { data } = res.data;
+            this.navs = data;
+        });
+    }
 };
 </script>
 
 <style lang="less" scoped>
 .box {
-  margin: auto;
-  width: 1000px;
-  display: flex;
-  Postleft {
-    width: 270px;
-  }
-  Postright {
-    width: 730px;
-  }
+    margin: auto;
+    width: 1000px;
+    display: flex;
+    Postleft {
+        width: 270px;
+    }
+    Postright {
+        width: 730px;
+    }
 }
->>>>>>> f7c5646a50790de95e0fe319c95ff1ba720ebae0
 </style>
 
